c85c566a4df3a6b908786b65cf22b9e7b49a875e
Nipunsen2k00 <nipunsenarathavi9@gmail.com>
2025-10-20 01:15:46 +0530

    Update signup.php

--- Hostel-Management-System-master/signup.php
+++ Hostel-Management-System-master/signup.php
@@ -20,7 +20,7 @@
     <!-- google fonts-->
     <link href="//fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-    <!-- /google fonts-->
+   
 
 </head>
 
